04229c5154cdb45873de00bbd10256b2073c900d
sametweb <sametmutevellioglu@gmail.com>
2019-12-14 09:39:33 -0800

    Character list, welcome page routes

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ char }) {
+  return (
+    <div>
+      <h3>{char.name}</h3>
+      <span>{char.species}</span>
+    </div>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [chars, setChars] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => setChars(res.data.results))
+      .catch(err => console.log(err));
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {chars.map(char => (
+        <CharacterCard char={char} />
+      ))}
     </section>
   );
 }
